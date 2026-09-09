7e09d37fdce3d652974bec188f171300709bb758
pandoraxdn <pandoraxdn@protonmail.com>
2026-09-09 08:26:50 -0600

    09-09-2026

--- mi_primer_app/App.tsx
+++ mi_primer_app/App.tsx
@@ -1,29 +1,9 @@
-import { View, Text } from "react-native";
-
-let nombre: string = "Rodrigo";
+import { PositionScreen } from "./src/screens/PositionScreen";
 
 const App = () => {
 
-  const grupo: string = "DSM-42";
-
   return (
-    <View
-      style={{
-        alignItems: "center",
-        alignContent: 'center'
-      }}
-    >
-      <Text
-        style={{
-          marginTop: 400,
-          fontSize: 50,
-          color: 'purple',
-          textAlign: 'center'
-        }}
-      >
-        Hola {nombre}, saludos al grupo: {grupo}
-      </Text>
-    </View>
+    <PositionScreen />
   );
 
 }
---
+++ mi_primer_app/src/screens/PositionScreen.tsx
@@ -0,0 +1,74 @@
+import { View, StyleSheet, useWindowDimensions } from "react-native";
+
+export const PositionScreen = () => {
+
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <View
+      style={ style.container }
+    >
+      <View
+        style={{
+          ...style.boxBlue,
+          backgroundColor: 'olive',
+          borderRadius: 150,
+          height: height * 0.2,
+          left: -20,
+          position: 'absolute',
+          top: -20,
+          width: width * 0.4
+        }}
+      />
+      <View
+        style={{
+          ...style.boxBlue,
+          //width: width,
+          //height: height * 0.33
+        }}
+      />
+      <View
+        style={{
+          ...style.boxViolet,
+          //width: width,
+          //height: height * 0.33
+        }}
+      />
+      <View
+        style={{
+          ...style.boxBlack,
+          //width: width,
+          //height: height * 0.33
+        }}
+      />
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row-reverse'
+    //alignContent: 'center',
+    //justifyContent: 'space-evenly'
+  },
+  boxBlue: {
+    //flex: 3
+    backgroundColor: 'blue',
+    height: 100,
+    width: 100
+  },
+  boxViolet: {
+    //flex: 3
+    backgroundColor: 'violet',
+    height: 100,
+    width: 100
+  },
+  boxBlack: {
+    //flex: 3
+    backgroundColor: 'black',
+    height: 100,
+    width: 100
+  }
+});
